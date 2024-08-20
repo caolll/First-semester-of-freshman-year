e4b3d94d2ccfb554baaa6e54beaef4c3b049d839
cao-jingwen1 <1879343740@qq.com>
2024-08-20 15:26:54 +0800

    test08.cpp

---
+++ test 08/test 08/test08.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+int main()
+{
+	while (1) {
+		system("color 5");
+		Sleep(10);
+		printf("÷ª“Ú               ");   
+		printf("ikun!!!");
+	}
+
+	return 0;
+}
