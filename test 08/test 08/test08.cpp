b1769c47978c84794af4674967fa6f53f3b66f01
cao-jingwen1 <1879343740@qq.com>
2024-08-23 00:05:43 +0800

    B2008

--- test 08/test 08/test08.cpp
+++ test 08/test 08/test08.cpp
@@ -1,14 +1,54 @@
-#include<stdio.h>
-#include<windows.h>
+#include <stdio.h>
 #include<stdlib.h>
-int main()
-{
-	while (1) {
-		system("color 5");
-		Sleep(10);
-		printf("只因               ");   
-		printf("ikun!!!");
-	}
+#include<windows.h>
+int main(){
+	int i,j;
+	int n = 12;
+	for (i = n / 2; i <= n;i+=2);
 
+	printf("*");
 	return 0;
 }
+//int main() {
+//    int i, j;
+//    int n = 6; // 心形的大小，可以根据需要调整
+//    // 上半部分心形图案
+//    for (i = n / 2; i <= n; i += 2) {
+//        // 打印空格
+//        for (j = 1; j < n - i; j += 2) {
+//            printf(" ");
+//        }
+//        // 打印左边的心形
+//        for (j = 1; j <= i; j++) {
+//            printf("*");
+//        }
+//        // 打印空格和右边的心形
+//        for (j = 1; j <= n - i; j++) {
+//            printf(" ");
+//        }
+//        for (j = 1; j <= i; j++) {
+//            printf("*");
+//        }
+//        printf("\n");
+//    }
+//    // 下半部分心形图案
+//    for (i = n; i >= 1; i -= 2) {
+//        // 打印空格
+//        for (j = 1; j <= n - i; j += 2) {
+//            printf(" ");
+//        }
+//        // 打印左边的心形
+//        for (j = 1; j <= i; j++) {
+//            printf("*");
+//        }
+//        // 打印空格和右边的心形
+//        for (j = 1; j <= n - i; j++) {
+//            printf(" ");
+//        }
+//        for (j = 1; j <= i; j++) {
+//            printf("*");
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
---
+++ test 09/test 09/test09.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,  b, c;
+	int jieguo;
+	scanf_s("%d%d%d", &a, &b, &c);
+	jieguo = (a + b) * c;
+	printf("%d", jieguo);
+	return 0;
+}
